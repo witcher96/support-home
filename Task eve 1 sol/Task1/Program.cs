b766f147a374f0316eca6a24108c4024065d2bfb
witcher96 <hasanaa@code.edu.az>
2022-01-22 19:43:23 +0400

    destek

---
+++ Task eve 1 sol/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int a = 3333;
+             if(a>=1000 && a < 10000)
+             {
+                    a = 70000 + a; //73333
+                 a = a * 10;         //733330
+                 a = a + 8;          //733338
+                 Console.WriteLine(a);
+             }
+             else
+             {
+                 //yanlis hal
+                 Console.WriteLine("4 reqemli deyil");
+
+             }
+           
+            
+
+        } 
+
+    }
+}
---
+++ Task eve 1 sol/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 3;
+            if (a < 10 && a>0)
+            {
+                a = a + a;
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine("Tekreqemli deyil");
+            }
+
+        }
+    }
+}
